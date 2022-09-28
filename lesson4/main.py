ec8d5ff91b877400a4076f0d820e5074ab945f95
daniilseva <danila.sevastianov2007@gmail.com>
2022-09-28 19:52:56 +0300

    4

---
+++ lesson4/main.py
@@ -0,0 +1,47 @@
+
+'''for c in range(1, 18):
+    print("Вы еще не совершеннолетний")
+    print("Вам недавно исполнилсь", c)
+    print("Осталось", 18 -c, "лет")
+print("Поздравляю")
+
+a = 5
+b = 6
+print(a + b)
+print(a * b)
+print(a - b)
+print(a / b)
+print(a // b)
+print(a ** b)
+print(a % b)
+
+text = """
+1
+3
+3
+"""
+print(text)
+
+txt = "Меня зовут Даниил"
+if "зовут" in txt:
+    print("Правда")
+
+age = 18
+
+print("My age is {}".format(age, "OK"))
+import random
+num1 = 0
+num2 = random.randint(0, 10)
+
+while num1 != num2:
+    num1 = int(input("Введите число: "))
+    if num1 == num2:
+        print("вы угадали")
+    elif num1 < num2:
+        print("Нужно больше")
+    elif num1 > num2:
+        print("Нужно меньше")
+print("Game over")
+'''
+
+
